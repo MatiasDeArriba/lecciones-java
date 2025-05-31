30f2621ac713a17f4366c44c30e781db4ab60820
Matias <matiasdearriba10@gmail.com>
2025-05-31 15:27:33 -0300

    proyecto inicial

---
+++ src/main/java/ar/com/mdearriba/LeccionesJavaApplication.java
@@ -0,0 +1,13 @@
+package ar.com.mdearriba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeccionesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeccionesJavaApplication.class, args);
+	}
+
+}
